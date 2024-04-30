63ce7cee3c645d3579d2f8cc85e2aea3434046d2
zarnigor <zarnigorasatova52@gmail.com>
2024-04-30 16:24:25 +0500

    feature

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
+// const openModalBtn = document.querySelector(".btn-open");
+// const closeModalBtn = document.querySelector(".btn-close");
+//
+// // close modal function
+// const closeModal = function () {
+//     modal.classList.add("hidden");
+//     overlay.classList.add("hidden");
+// };
+//
+// // close the modal when the close button and overlay is clicked
+// closeModalBtn.addEventListener("click", closeModal);
+// overlay.addEventListener("click", closeModal);
+//
+// // close modal when the Esc key is pressed
+// document.addEventListener("keydown", function (e) {
+//     if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+//         closeModal();
+//     }
+// });
+//
+// // open modal function
+// const openModal = function () {
+//     modal.classList.remove("hidden");
+//     overlay.classList.remove("hidden");
+// };
+// // open modal event
+// openModalBtn.addEventListener("click", openModal);
+
+
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
+const openModalBtns = document.querySelectorAll(".btn-open");
 const closeModalBtn = document.querySelector(".btn-close");
 
 // close modal function
@@ -25,5 +56,8 @@ const openModal = function () {
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
-// open modal event
-openModalBtn.addEventListener("click", openModal);
+
+// open modal event for each button
+openModalBtns.forEach(function (btn) {
+    btn.addEventListener("click", openModal);
+});
